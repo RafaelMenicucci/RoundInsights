3aea2554f56ccfd88b68c00994003d354e8e0bdc
Rafael Alves <rafa_alves96@hotmail.com>
2023-06-09 16:22:10 -0300

    fix signup

--- core/views.py
+++ core/views.py
@@ -14,7 +14,7 @@ def signup(request):
         pass1 = request.POST['pass1']
         pass2 = request.POST['pass2']
 
-        myuser = User.objects.create_user(username,pass1)
+        myuser = User.objects.create_user(username,'',pass1)
         
         myuser.save()
 
