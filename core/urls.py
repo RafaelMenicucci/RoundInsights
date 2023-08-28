f90c145be8bb1104a0760b9873e105cc1c381316
Rafael Alves <rafa_alves96@hotmail.com>
2023-08-28 19:39:34 -0300

    minor fixes layout

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,7 @@ from . import views
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('', views.signup, name="signup"),
